d27234954efb4a2822efed3e336e6e5ed900fe41
diormazabal <164242847+diormazaball@users.noreply.github.com>
2024-09-12 17:07:32 -0300

    Base app
    ng --version
    18.1.3
    ionic -v
    7.2.0
    node --version
    v20.16.0

---
+++ appzoolife/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appzoolife',
+  webDir: 'www'
+};
+
+export default config;
